9e1f0156432a3b29366ecd391d3f3a171d62d30d
DollHair <yigerenchihuntun@qq.com>
2019-05-23 13:50:21 +0800

    commit

--- assets/Script/gaming/gamingcontroller.ts
+++ assets/Script/gaming/gamingcontroller.ts
@@ -40,72 +40,104 @@ export default class GamingController extends cc.Component {
     @property(cc.Node)
     bg: cc.Node = null;
 
+    @property(cc.Label)
+    score1: cc.Label = null;
+
+    @property(cc.Label)
+    title: cc.Label = null;
+
 
 
     time: number = 0;
 
     flag: number = 0;
 
+    score: number = 0;
+
+    title1: number = 0;
+
+
+
     // LIFE-CYCLE CALLBACKS:
 
     // onLoad () {}
 
     start () {
-
+        this.score = 0;
+        this.randtitle();
     }
 
     update (dt:number) {
-        let timeTmp = this.time + dt;
-        this.time = timeTmp;
+        //let timeTmp = this.time + dt;
+        //this.time = timeTmp;
+
 
         this.move(this.cm1);
         this.move(this.bd1);
         this.move(this.ttq1);
         this.move(this.beg1);
+        
+        if (this.flag == this.title1){
+            this.score += 5;
+            this.randtitle();
+        }
 
-        if (this.flag == 1){
+        if(this.score > 100){
             this.gameover();
         }
-        /*
-        else{
-            let cmX = this.cm1.x;
-            this.cm1.x = cmX + 5;            
-        }
-        let bdX = this.bd1.x;
-        this.bd1.x = bdX + 5;
 
-        let ttqX = this.ttq1.x;
-        this.ttq1.x = ttqX + 5;
-
-        let begX = this.beg1.x;
-        this.beg1.x = begX + 5;
-        
-        if (this.cm1.x > 687){
-            this.cm1.x = -887;
-        }
-        
-        if (this.bd1.x > 687){
-            this.bd1.x = -887;
-        }
-        
-        if (this.ttq1.x > 687){
-            this.ttq1.x = -887;
-        }
-        
-        if (this.beg1.x > 687){
-            this.beg1.x = -887;
-        }*/
+        this.score1.string = String(this.score);
      }
+
+     randtitle(){
+        this.title1 = Math.ceil(Math.random()*4);
+        switch(this.title1)
+        {
+            case(1): this.title.string = "草莓";break;
+            case(2): this.title.string = "布丁";break;
+            case(3): this.title.string = "甜甜圈";break;
+            case(4): this.title.string = "饼干";break;
+        }
+    }
+
      move(wuti:cc.Node){
         let wutiX = wuti.x;
-        wuti.x = wutiX + 5;
+        if(this.score < 20){
+            wuti.x = wutiX + 5;
+        }
+        else if(20 <= this.score && this.score < 40){
+            wuti.x = wutiX + 7;
+        }
+        else if(40 <= this.score && this.score < 60){
+            wuti.x = wutiX + 11;
+        }
+        else if(60 <= this.score && this.score < 80){
+            wuti.x = wutiX + 13;
+        }
+        else if(80 <= this.score){
+            wuti.x = wutiX + 15;
+        }
         if(wuti.x > 687){
             wuti.x = -887;
         }
      }
-     onbutton(){
-         this.flag = 1;
+
+     onbutton1(){
+        this.flag = 1;
      }
+
+     onbutton2(){
+         this.flag = 2;
+     }
+
+     onbutton3(){
+         this.flag = 3;
+     }
+
+     onbutton4(){
+        this.flag = 4;
+     }
+
      gameover() {
          cc.director.loadScene("gameover");
      }
--- temp/quick-scripts/assets/Script/gaming/gamingcontroller.js
+++ temp/quick-scripts/assets/Script/gaming/gamingcontroller.js
@@ -26,63 +26,85 @@ var GamingController = /** @class */ (function (_super) {
         _this.ttq1 = null;
         _this.beg1 = null;
         _this.bg = null;
+        _this.score1 = null;
+        _this.title = null;
         _this.time = 0;
         _this.flag = 0;
+        _this.score = 0;
+        _this.title1 = 0;
         return _this;
     }
     // LIFE-CYCLE CALLBACKS:
     // onLoad () {}
     GamingController.prototype.start = function () {
+        this.score = 0;
+        this.randtitle();
     };
     GamingController.prototype.update = function (dt) {
-        var timeTmp = this.time + dt;
-        this.time = timeTmp;
+        //let timeTmp = this.time + dt;
+        //this.time = timeTmp;
         this.move(this.cm1);
         this.move(this.bd1);
         this.move(this.ttq1);
         this.move(this.beg1);
-        if (this.flag == 1) {
+        if (this.flag == this.title1) {
+            this.score += 5;
+            this.randtitle();
+        }
+        if (this.score > 100) {
             this.gameover();
         }
-        /*
-        else{
-            let cmX = this.cm1.x;
-            this.cm1.x = cmX + 5;
+        this.score1.string = String(this.score);
+    };
+    GamingController.prototype.randtitle = function () {
+        this.title1 = Math.ceil(Math.random() * 4);
+        switch (this.title1) {
+            case (1):
+                this.title.string = "草莓";
+                break;
+            case (2):
+                this.title.string = "布丁";
+                break;
+            case (3):
+                this.title.string = "甜甜圈";
+                break;
+            case (4):
+                this.title.string = "饼干";
+                break;
         }
-        let bdX = this.bd1.x;
-        this.bd1.x = bdX + 5;
-
-        let ttqX = this.ttq1.x;
-        this.ttq1.x = ttqX + 5;
-
-        let begX = this.beg1.x;
-        this.beg1.x = begX + 5;
-        
-        if (this.cm1.x > 687){
-            this.cm1.x = -887;
-        }
-        
-        if (this.bd1.x > 687){
-            this.bd1.x = -887;
-        }
-        
-        if (this.ttq1.x > 687){
-            this.ttq1.x = -887;
-        }
-        
-        if (this.beg1.x > 687){
-            this.beg1.x = -887;
-        }*/
     };
     GamingController.prototype.move = function (wuti) {
         var wutiX = wuti.x;
-        wuti.x = wutiX + 5;
+        if (this.score < 20) {
+            wuti.x = wutiX + 5;
+        }
+        else if (20 <= this.score && this.score < 40) {
+            wuti.x = wutiX + 7;
+        }
+        else if (40 <= this.score && this.score < 60) {
+            wuti.x = wutiX + 11;
+        }
+        else if (60 <= this.score && this.score < 80) {
+            wuti.x = wutiX + 13;
+        }
+        else if (80 <= this.score) {
+            wuti.x = wutiX + 15;
+        }
         if (wuti.x > 687) {
             wuti.x = -887;
         }
     };
-    GamingController.prototype.onbutton = function () {
+    GamingController.prototype.onbutton1 = function () {
         this.flag = 1;
+    };
+    GamingController.prototype.onbutton2 = function () {
+        this.flag = 2;
+    };
+    GamingController.prototype.onbutton3 = function () {
+        this.flag = 3;
+    };
+    GamingController.prototype.onbutton4 = function () {
+        this.flag = 4;
     };
     GamingController.prototype.gameover = function () {
         cc.director.loadScene("gameover");
@@ -114,6 +136,12 @@ var GamingController = /** @class */ (function (_super) {
     __decorate([
         property(cc.Node)
     ], GamingController.prototype, "bg", void 0);
+    __decorate([
+        property(cc.Label)
+    ], GamingController.prototype, "score1", void 0);
+    __decorate([
+        property(cc.Label)
+    ], GamingController.prototype, "title", void 0);
     GamingController = __decorate([
         ccclass
     ], GamingController);
